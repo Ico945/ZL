ffcc3d0075fb850797843f87774669d81aefdc27
yehengchao <yhc@yhc-iMac.local>
2019-01-16 23:05:06 +0800

    压缩全部图片

--- app/public/javascripts/header.js
+++ app/public/javascripts/header.js
@@ -2,6 +2,10 @@ var nav = document.querySelector('nav.nav');
 let navItems = Array.prototype.slice.call(nav.children);
 let pathname = document.location.pathname;
 navItems.forEach(item => {
-    if (item.pathname === pathname)
-        $(item).toggleClass('is-active');
+    let itemClasses = item.classList;  
+    if (item.pathname === pathname) {
+        itemClasses.add('is-active');
+    } else {
+        itemClasses.remove('is-active');
+    }
 });
